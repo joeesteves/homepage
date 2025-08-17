0a50660cec8a268a00a5a6d47bbdc330d3040d2b
Joe <jose@esteves.ar>
2025-08-17 18:17:29 -0300

    engineer

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,7 @@ export const Header = () => (
   <div className="header">
     <img src={profileLogo} className="logo" alt="Profile logo" />
     <h1>Joe Esteves</h1>
-    <h5>Full-Stack Team Lead</h5>
+    <h5>Full-Stack Engineer</h5>
     <h5>joe@esteves.ar</h5>
     <div className="header__social-links">
       <a href="https://github.com/joeesteves" target="_blank">
