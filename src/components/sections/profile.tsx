81bf59fb47c9a0a7715c0fc30a942765938b6434
Joe Esteves <joe@esteves.ar>
2025-10-15 08:38:37 -0300

    fix

--- src/components/sections/profile.tsx
+++ src/components/sections/profile.tsx
@@ -9,10 +9,10 @@ const description = (
       I've successfully guided teams through complex system migrations, legacy code modernization, and greenfield projects across multiple backend, from Rails monoliths to Elixir's to Node.js. My independent work has given me expertise in making architectural decisions under real-world constraints, choosing the optimal (generally the simplest) solution for specific business requirements and designing deployment pipelines across different platforms.
     </p>
     <p>
-      I love building and scaling full-stack applications from the ground up, making strategic decisions about modern architectures: microservices communication via REST/GraphQL APIs, real-time features with WebSockets, message queuing with Kafka, and containerized deployments with Docker/Kubernetes. My independent development experience spans the entire DevOps pipeline—from initial architecture design and coding to CI/CD implementation, infrastructure provisioning, and production monitoring.
+      I love building and scaling full-stack applications from the ground up, making strategic decisions about modern architectures: microservices communication via REST/GraphQL APIs, real-time features with WebSockets, message queuing with Kafka, and containerized deployments with Docker/Kubernetes. My independent development experience spans the entire DevOps pipeline, from initial architecture design and coding to CI/CD implementation, infrastructure provisioning, and production monitoring.
     </p>
     <p>
-      Beyond TypeScript, I bring valuable experience in Elixir/Phoenix and Ruby on Rails, having maintained and developed over large codebases. I'm passionate about programming in general (my personal programming bookshel can confirm :)) I keep learning almost every day, new patters, blockchain, languages, paradigms, IA tooling since this is what I love to do.
+      Beyond TypeScript, I bring valuable experience in Elixir/Phoenix and Ruby on Rails, having maintained and developed over large codebases. I'm passionate about programming in general (my personal programming bookshell 📖 can confirm) I keep learning almost every day, new patters, blockchain, languages, paradigms, IA tooling since this is what I love to do.
     </p>
   </div>
 );
